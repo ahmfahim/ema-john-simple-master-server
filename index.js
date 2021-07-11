1fc8dbf35f2f8095889c6b0df17a826bc7168a9f
ahmfahim <fahimmorshed2019@gmail.com>
2021-07-11 17:36:12 +0600

    master

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const { MongoClient } = require('mongodb');
 const port = 5000;
 require('dotenv').config()
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b99uy.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express()
 app.use(cors())
 app.use(bodyParser.json());
 
+app.get('/', (req, res) => {
+    res.send('connected!')
+})
 
-
-//connection to mongoDB server
-const { MongoClient } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b99uy.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 client.connect(err => {
     const productsCollection = client.db("emaJohnStore").collection("products");
@@ -65,10 +67,5 @@ client.connect(err => {
 
     // --------
 });
-
-
-app.get('/', (req, res) => {
-    res.send('connected!')
-})
 
 app.listen(process.env.PORT || port);
